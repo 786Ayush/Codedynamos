c7ae614fc42177ebb968244b06432d78e57fee08
786Ayush <88676009+786Ayush@users.noreply.github.com>
2024-01-05 13:56:05 +0530

    Frontend

---
+++ src/Component/Foot.jsx
@@ -0,0 +1,204 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  FaInstagram,
+  FaLinkedin,
+  FaWhatsapp,
+  FaFacebook,
+} from "react-icons/fa";
+
+const Footer = () => {
+  const arrowIcon = "→"; // Unicode character for right arrow
+
+  return (
+    <footer className="bg-gray-800 text-white">
+      <div className="container mx-auto flex flex-col justify-between  md:flex-row mb-5">
+        {/* Big Logo Photo */}
+        <div className=" w-full md:w-1/4 ">
+          <div className="flex items-center px-10">
+            <img src="/Logo.png" alt="Company Logo" className="w-32 h-32 " />
+          </div>
+          <div className=" px-10">
+            <h1 className="text-lg md:text-xl lg:text-2xl font-bold">
+              Devbuzzhub Internship
+            </h1>
+            <h4>Phone: 1234567890</h4>
+            <h4>Gmail: abc@gmail.com</h4>
+          </div>
+        </div>
+
+        {/* Useful Links Section */}
+        <div className="w-full md:w-1/4 mb-4 md:mb-0 md: mt-16">
+          <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-gray-300 text-center">
+            Useful Links
+          </h3>
+          <ul className="text-sm md:text-base lg:text-lg  px-5">
+            <li>
+              <Link to="/" className="hover:text-gray-300 text-white">
+                {arrowIcon} Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="hover:text-gray-300 text-white">
+                {arrowIcon} About Us
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/internship-domains"
+                className="hover:text-gray-300 text-white"
+              >
+                {arrowIcon} Internship Domains
+              </Link>
+            </li>
+            <li>
+              <Link to="/roadmaps" className="hover:text-gray-300 text-white">
+                {arrowIcon} Roadmaps
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:text-gray-300 text-white">
+                {arrowIcon} Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Internship Domains Section */}
+        <div className="w-full md:w-1/4 mb-4 md:mb-0 md: mt-16">
+          <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-gray-300 text-center">
+            Internship Domains
+          </h3>
+          <ul className="text-sm md:text-base lg:text-lg px-5">
+            <li>
+              <Link
+                to="/web-development"
+                className="hover:text-gray-300 text-white"
+              >
+                {arrowIcon} Web Development
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/graphic-design"
+                className="hover:text-gray-300 text-white"
+              >
+                {arrowIcon} Graphic Design
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/data-science"
+                className="hover:text-gray-300 text-white"
+              >
+                {arrowIcon} Data Science
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/artificial-intelligence"
+                className="hover:text-gray-300 text-white"
+              >
+                {arrowIcon} Artificial Intelligence
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/python-development"
+                className="hover:text-gray-300 text-white"
+              >
+                {arrowIcon} Python Development
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/android-development"
+                className="hover:text-gray-300 text-white"
+              >
+                {arrowIcon} Android Development
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/java-development"
+                className="hover:text-gray-300 text-white"
+              >
+                {arrowIcon} Java Development
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/content-writer"
+                className="hover:text-gray-300 text-white"
+              >
+                {arrowIcon} Content Writer
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Inbox Section */}
+        <div className="w-full md:w-1/4 md: mt-16">
+          <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-gray-300 text-center">
+            Join Our Internship
+          </h3>
+          <p className="text-sm md:text-base lg:text-lg  px-5 text-center">
+            Join the Virtual Internship Program in cutting-edge technologies
+            with Devbuzzhub.
+          </p>
+        </div>
+      </div>
+      <div className="bg-[var(--nav-color)] flex flex-col md:flex-row p-3 text-sm justify-around">
+        <div className="text-white mb-2 md:mb-0 text-center md:text-left">
+          <p>&copy; Copyright AnotherCompany Internship. All Rights Reserved</p>
+          <p>
+            Designed by <span className="text-blue-400">Devbuzzhub </span>
+          </p>
+        </div>
+        <div className="flex justify-center md:justify-end  items-center">
+          {/* Instagram Button */}
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white p-3 rounded-full"
+          >
+            <FaInstagram className="w-6 h-6 md:w-8 md:h-8 hover:text-gray-300" />
+          </a>
+
+          {/* LinkedIn Button */}
+          <a
+            href="https://www.linkedin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white p-3 rounded-full"
+          >
+            <FaLinkedin className="w-6 h-6 md:w-8 md:h-8 hover:text-gray-300" />
+          </a>
+
+          {/* WhatsApp Button */}
+          <a
+            href="https://wa.me/1234567890"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white p-3 rounded-full"
+          >
+            <FaWhatsapp className="w-6 h-6 md:w-8 md:h-8 hover:text-gray-300" />
+          </a>
+
+          {/* Facebook Button */}
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white p-3 rounded-full"
+          >
+            <FaFacebook className="w-6 h-6 md:w-8 md:h-8 hover:text-gray-300" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { IoMenu, IoClose } from "react-icons/io5";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  const LinksList = () => (
+    <div className="md:hidden absolute top-16 right-0 p-4 bg-[var(--nav-color)] w-full">
+      <ul>
+        <li className="w-full hover:bg-zinc-600 rounded text-center">
+          <Link to="/" className="text-white  ">
+            Home
+          </Link>
+        </li>
+        <li className="w-full hover:bg-zinc-600 rounded text-center">
+          <Link to="/about" className="text-white ">
+            About us
+          </Link>
+        </li>
+        <li className="w-full hover:bg-zinc-600 rounded text-center">
+          <Link to="/internship" className="text-white ">
+            Internship domains
+          </Link>
+        </li>
+        <li className="w-full hover:bg-zinc-600 rounded text-center">
+          <Link to="/roadmap" className="text-white">
+            Roadmap
+          </Link>
+        </li>
+        <li className="w-full hover:bg-zinc-600 rounded text-center">
+          <Link to="/contact" className="text-white">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+
+  return (
+    <nav
+      className={`relative flex justify-around items-center text-white`}
+      style={{ backgroundColor: "var(--nav-color)" }}
+    >
+      {/* Logo on the left */}
+      <div className="flex items-center">
+        <img src="/Logo.png" alt="Logo" className="h-16 w-16 mr-2" />
+        {/* <span className="text-xl font-bold">YourLogo</span> */}
+      </div>
+
+      {/* Buttons in the center (visible on larger screens) */}
+      <div className={`hidden md:flex gap-4 ${showLinks ? "flex" : "hidden"}`}>
+        <Link
+          to="/"
+          className="text-white px-4 py-2 hover:bg-zinc-600 rounded"
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="text-white px-4 py-2 hover:bg-zinc-600 rounded"
+        >
+          About us
+        </Link>
+        <Link
+          to="/internship"
+          className="text-white px-4 py-2 hover:bg-zinc-600 rounded"
+        >
+          Internship domains
+        </Link>
+        <Link
+          to="/roadmap"
+          className="text-white px-4 py-2 hover:bg-zinc-600 rounded"
+        >
+          Roadmap
+        </Link>
+        <Link
+          to="/contact"
+          className="text-white px-4 py-2 hover:bg-zinc-600 rounded"
+        >
+          Contact
+        </Link>
+      </div>
+
+      <div className="flex items-center">
+        {/* Login button on the right */}
+        <div>
+          <button
+            className="text-white px-4 py-2 rounded m-2"
+            style={{ backgroundColor: "var(--button)" }}
+          >
+            Login
+          </button>
+        </div>
+        {/* Button to show/hide links on smaller screens */}
+        <div className="md:hidden">
+          <button onClick={toggleLinks} className="text-white">
+            {showLinks ? <IoClose size={30} /> : <IoMenu size={30} />}
+          </button>
+          {showLinks && <LinksList />}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
